3da8b14b93f404216c644773de5a66d638066a0d
studstone <nikolas2996@gmail.com>
2022-03-13 19:00:44 +0300

    fix form number

--- js/main.js
+++ js/main.js
@@ -65,9 +65,9 @@ $(document).ready(function () {
   });
 });
 
-let selector = document.querySelectorAll('input[type="tel"]');
-let im = new Inputmask("+7 (999) 999-99-99");
-im.mask(selector);
+//let selector = document.querySelectorAll('input[type="tel"]');
+//let im = new Inputmask("+7 (999) 999-99-99");
+//im.mask(selector);
 
 const swiper = new Swiper(".categories-swiper", {
   //loop: false,
